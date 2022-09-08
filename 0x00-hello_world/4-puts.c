2e1373bba0175b9eadaf8a2d8e799dd2eac1e4a7
ijeoma ogbuefi <jessickaokoye@gmail.com>
2022-09-08 15:22:25 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,23 +4,19 @@
 
 /**
  *
- * * main - print the string in the put function
+ *  * main - Entry point
  *
- * *
+ *   *
  *
- * * Description: using the main function
+ *    * Return: Always 0 (Success)
  *
- * * this program prints "Programming is like building a multilingual puzzle
- *
- * * Return: 0
- *
- * */
+ *     */
 
 int main(void)
 
 {
 
-		printf("with proper grammar, but the outcome is a piece of art,\n");
+		puts("\"Programming is like building a multilingual puzzle");
 
 			return (0);
 
