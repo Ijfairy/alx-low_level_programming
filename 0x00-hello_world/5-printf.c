89b26227fba6e9a036e63e90bef1b89040939e7a
ijeoma ogbuefi <jessickaokoye@gmail.com>
2022-09-08 17:58:59 -0700

    Task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,9 @@
 #include <stdio.h>
 
-
 /**
-* main - print the string in the put function
+* main - print a text
 *
-* Description: using the main function
-* this program prints "programming is like building a multilingual puzzle
-* Return: 0
+* Return: Always 0 (Success)
 */
 int main(void)
 {
