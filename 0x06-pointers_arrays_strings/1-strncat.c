2c3129de6e528b5abca365e6c5600c4ff5bbaa2e
ijeoma ogbuefi <jessickaokoye@gmail.com>
2022-09-21 16:24:57 -0700

    Task 1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -10,13 +10,13 @@
 char *_strncat(char *dest, char *src, int n)
 {
 
-	int index = 0, dest len = 0;
+	int index = 0, dest_len = 0;
 
 	while (dest[index++])
-		dest_lent++;
+		dest_len++;
 
 	for (index = 0; src[index] && index < n; index++)
-		dest[dest_lent++] = src[index];
+		dest[dest_len++] = src[index];
 
 	return (dest);
 }
