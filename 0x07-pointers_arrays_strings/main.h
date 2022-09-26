d8038df640692b9254f2ab717ccea53bd4c2d930
ijeoma ogbuefi <jessickaokoye@gmail.com>
2022-09-26 16:31:43 -0700

    Task 0

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,12 +1,6 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef FILE_MAIN
+#define FILE_MAIN
 
-/**
- * void prototypes()
- * int prototypes()
- */
-
-int _putchar(char);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
@@ -14,7 +8,8 @@ unsigned int _strspn(char *s, char *accept);
 char *strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
+int _putchar(char c);
 void print_diagsums(int *a, int size);
 void set_string(char **s, char *to);
 
-#endif /*MAIN_H*/
+#endif
